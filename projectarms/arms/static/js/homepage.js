340b9a5be3973719db6fcddb907042ccd01f7d1d
leam123 <garcia.leamor1@gmail.com>
2020-11-18 21:35:28 +0800

    Homepage Change 1.0

--- projectarms/arms/static/js/homepage.js
+++ projectarms/arms/static/js/homepage.js
@@ -1,10 +1,11 @@
+//Book Slider
 $(document).ready(function() {
   $('.slider').slick({
     centerMode: true,
-    centerPadding: '60px',
+    centerPadding: '100px',
     slidesToShow: 3,
     nextArrow: '<button type="button" class="btn btn-next"><i class="fa fa-arrow-right" aria-hidden="true"></i></button>',
-    /*responsive: [
+    responsive: [
       {
         breakpoint: 768,
         settings: {
@@ -23,7 +24,7 @@ $(document).ready(function() {
           slidesToShow: 1
         }
       }
-    ],*/
+    ],
   });
 });
 
@@ -41,6 +42,36 @@ $(document).ready(function() {
 }*/
 
 
+/*function allImages(){
+    var imgs = document.getElementsByClassName("thumbnail");
+    var search = document.getElementById("searchBox").value;
+    var title = [];
+    var display = document.getElementById("searched");
+    var cl;
+
+    for(var i = 0; i < imgs.length; i++) {
+        title[i] = imgs[i].getAttribute('title');
+
+
+        if(title[i] != null){
+          if(title[i].toLowerCase() == search.toLowerCase()){
+            //console.log(imgs[i].src);
+            display.appendChild(imgs[i]);
+            $(".text").hide();
+            $(".btn-next").hide();
+          }else{
+
+          }
+        }if(search == ""){
+          display.remove();
+
+          //console.log("null");
+        }
+
+    }
+}*/
+
+//Live Search -> not yet done
 $(document).ready(function () {
 
   /*$(".thumbnail").hide();*/
@@ -50,7 +81,7 @@ $(document).ready(function () {
       // Retrieve the input field text 
       var filter = $(this).val();
       //var count = 0;
-
+      var display = document.getElementById("searched");
 
       // Loop through the captions div 
       $(".thumbnail").each(function(){
@@ -60,16 +91,16 @@ $(document).ready(function () {
               $(this).fadeOut();
               $(".text").hide();
               $(".btn-next").hide();
-
           // Show the div item if the phrase matches 
           } else {
-              $(this).show();
-              $(".text").show();
-              $(".btn-next").show();
+              $("#result").html("Results"+"<br>");
+              $(this).show().appendTo("#searched");
+              
+              //$(".text").show();
+              //$(".btn-next").show();
               //count++;
           }
       });
-
       // Update the count
       /*var numberItems = count;
       $("#filter-count").text("Number of Filter = "+count);*/
@@ -97,19 +128,28 @@ $(document).ready(function () {
   }
 }*/
 
-/*function searchbyname(ele) {
-  // Declare variabl
-  const filter = jQuery(ele).val();
-
-  $('.slider').slick('slickUnfilter');
-  $('.slider').slick('slickFilter', function() {
-    let content = jQuery(this).find("img").text().toLowerCase();
-    return content.indexOf(filter) > -1;
-  });
-}*/
-
 /*function openPDF(){
   var file = document.getElementById('pdfFile').val();
   window.open(file, "_blank");
 }*/
 
+
+//Copy the auto generated citation to clipboard
+function copyCitation() {
+  /* Get the text field */
+  var copyText = document.getElementById("citationText");
+
+  /* Select the text field */
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); /*For mobile devices*/
+
+  /* Copy the text inside the text field */
+  document.execCommand("copy");
+}
+
+//Call the Toast for citation
+$(document).ready(function(){
+  $("#showToast").click(function(){
+    $('.toast').toast('show');
+  });
+});
