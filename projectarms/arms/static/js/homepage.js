43dec8b293aadf8f9dec9e4e854ca76f5572ce5c
leam123 <garcia.leamor1@gmail.com>
2020-11-23 19:28:45 +0800

    ...

--- projectarms/arms/static/js/homepage.js
+++ projectarms/arms/static/js/homepage.js
@@ -32,26 +32,9 @@ $(document).ready(function() {
           slidesToScroll: 1
         }
       }
-      // You can unslick at a given breakpoint now by adding:
-      // settings: "unslick"
-      // instead of a settings object
     ],
   });
 });
-
-
-
-/*function searchbyname(ele) {
-  // Declare variabl
-  const filter = jQuery(ele).val();
-
-  $('.slider').slick('slickUnfilter');
-  $('.slider').slick('slickFilter', function() {
-    let content = jQuery(this).find("img").text().toLowerCase();
-    return content.indexOf(filter) > -1;
-  });
-}*/
-
 
 /*function allImages(){
     var imgs = document.getElementsByClassName("thumbnail");
@@ -91,7 +74,6 @@ $(document).ready(function () {
 
       // Retrieve the input field text 
       var filter = $(this).val();
-      //var count = 0;
       var display = document.getElementById('display');
 
       // Loop through the captions div 
@@ -129,30 +111,6 @@ $(document).ready(function () {
       $("#filter-count").text("Number of Filter = "+count);*/
    });
 });
-
-
-
-
-
-/*function myFunction() {
-  // Declare variables
-  var input, filter, ul, li, a, i, txtValue;
-  input = document.getElementById("searchBox");
-  filter = input.value.toLowerCase();
-  ul = document.getElementById("menu");
-  li = ul.getElementsByTagName("li");
-
-  // Loop through all list items, and hide those who don't match the search query
-  for (i = 0; i < li.length; i++) {
-    a = li[i].getElementsByTagName("title")[0];
-    txtValue = a.textContent || a.innerText;
-    if (txtValue.toLowerCase().indexOf(filter) > -1) {
-      li[i].style.display = "";
-    } else {
-      li[i].style.display = "none";
-    }
-  }
-}*/
 
 function openPDF(){
   var file = document.getElementById('pdfFile').getAttribute('href');
